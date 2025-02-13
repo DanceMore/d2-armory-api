fa09d0420164cbbd102ab78b675ceb8ac1a1e9f0
DanceMore <dancemore@protonmail.com>
2025-02-12 17:35:04 -0800

    poc list

--- internal/httpserver/list_characters.go
+++ internal/httpserver/list_characters.go
@@ -3,12 +3,28 @@ package httpserver
 
 import (
 	"net/http"
-	
+	"fmt"
+	"os"
+	"sort"
+	"path/filepath"
+	"time"
+
 	"github.com/go-chi/chi"
 )
 
 // listCharactersHandler is used to list available characters.
-type listCharactersHandler struct{}
+type listCharactersHandler struct {
+	encoder          *encoder
+	d2sPath          string
+	characterService characterService
+}
+
+// Add this new struct to hold file info
+type CharacterFileInfo struct {
+	Name          string    `json:"name"`
+	LastModified  time.Time `json:"last_modified"`
+}
+
 
 func newListCharactersHandler() *listCharactersHandler {
 	return &listCharactersHandler{}
@@ -19,7 +35,41 @@ func (h *listCharactersHandler) Routes(router chi.Router) {
 }
 
 func (h *listCharactersHandler) list(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement actual listing logic
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("List all characters"))
+    // Read directory entries
+    files, err := os.ReadDir(h.d2sPath)
+    if err != nil {
+        h.encoder.Error(w, fmt.Errorf("failed to read directory: %v", err))
+        return
+    }
+
+    var characterFiles []CharacterFileInfo
+
+    for _, file := range files {
+        // Skip directories and only process character files (no extension needed)
+        if !file.IsDir() {
+            fullPath := filepath.Join(h.d2sPath, file.Name())
+
+            // Get full file info including modification time
+            fileInfo, err := os.Stat(fullPath)
+            if err != nil {
+                continue // Skip files that can't be accessed
+            }
+
+            characterFiles = append(characterFiles, CharacterFileInfo{
+                Name:          file.Name(),
+                LastModified:  fileInfo.ModTime().UTC(), // Get UTC time for consistency
+            })
+        }
+    }
+
+    // Sort by last modified time descending (most recent first)
+    sort.SliceStable(characterFiles, func(i, j int) bool {
+        return characterFiles[i].LastModified.After(characterFiles[j].LastModified)
+    })
+
+    h.encoder.Response(w, struct {
+        Characters []CharacterFileInfo `json:"characters"`
+    }{
+        Characters: characterFiles,
+    })
 }
